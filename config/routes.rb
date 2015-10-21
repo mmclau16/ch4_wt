24e156df1f7c19486de8552f75dfd0cd246fcd53
marissamclaughlin <mmclau16@uncc.edu>
2015-10-21 23:45:46 +0000

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :movies
+  root :to => redirect( '/movies')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +16,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
